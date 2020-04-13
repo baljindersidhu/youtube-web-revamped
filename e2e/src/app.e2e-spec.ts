79a1bd4d734cd19f7eb503e39af2adbeab06d90e
Baljinder Singh Sidhu <Baljinder.S.Sidhu@aexp.com>
2020-04-13 10:39:01 +0530

    Project Setup Complete

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-scaffold!');
+    expect(page.getTitleText()).toEqual('Welcome to youtube-web-revamped!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-scaffold'`, () => {
+  it(`should have as title 'youtube-web-revamped'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-scaffold');
+    expect(app.title).toEqual('youtube-web-revamped');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-scaffold!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to youtube-web-revamped!');
   });
 });
